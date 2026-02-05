796c06b99e722711e90f0d58c4cff4082f6fcfd9
HARI HARA SUDHAN <sudhanabirami007@gmail.com>
2026-02-05 14:52:35 +0530

    local audio

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from IPython.display import Audio, display
 from src.config.settings import MODEL_ID, DEVICE
 from src.models.language_classifier import load_language_classifier
-from src.audio.audio_loader import load_audio_from_url
+from src.audio.audio_loader import load_audio_from_file
 from src.services.inference import classify_language
 
 
@@ -11,9 +11,11 @@ def main():
     classifier = load_language_classifier(MODEL_ID, DEVICE)
     print("Model loaded successfully")
 
-    url = "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac"
+    # url = "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac"
+    path = "Testing/audio/harvard.wav"
 
-    audio, sr = load_audio_from_url(url)
+    # audio, sr = load_audio_from_url(url) 
+    audio, sr = load_audio_from_file(path)
 
     display(Audio(audio, rate=sr))
 
@@ -21,7 +23,6 @@ def main():
 
     for r in results:
         print(f"{r['label']:15s} {r['score']:.4f}")
-
 
 if __name__ == "__main__":
     main()
